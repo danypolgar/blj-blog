522bcdadb8c3b560688ec9d61e4e20fb34a590f4
Daniel Polgar <danypolgar@gmail.com>
2018-11-28 17:01:38 +0100

    post date, other pages and post images complete

--- blog/index.php
+++ blog/index.php
@@ -40,8 +40,11 @@ $isPostValid = true;
     
 if (isset($_POST["speichern"])) {
     $name = $_POST['name'] ??'';
-    $title = $_POST['title']??'';
-    $post = $_POST['post']??'';
+    $title = $_POST['title'] ??'';
+    $post = $_POST['post'] ??'';
+    $image = trim($_POST['image'] ??'');
+
+
     if(empty($name)){
         ?> <p class=errorbox>Bitte geben Sie Ihr Name an.</p> <?php
         $isNameValid = false;
@@ -56,11 +59,10 @@ if (isset($_POST["speichern"])) {
     }
     
 
-
     
     if($isNameValid===true && $isTitleValid===true && $isPostValid===true){
-        $stmt = $dbh->prepare("INSERT INTO posts (created_by, post_title, post_text) VALUES(:name, :title, :post)");
-        $stmt->execute([':name' => $name, ':title' => $title, ':post' => $post]);
+        $stmt = $dbh->prepare("INSERT INTO posts (created_by, post_title, post_text, post_image) VALUES(:name, :title, :post, :post_image)");
+        $stmt->execute([':name' => $name, ':title' => $title, ':post' => $post, ':post_image' => $image]);
     }
 }
 ?>
@@ -75,6 +77,8 @@ if (isset($_POST["speichern"])) {
         <label for="title"> Title:</label><input id="title" type="text" name="title"><br>
         <label for="post"> Text:</label><textarea id="post"  name="post" rows="10"></textarea><br>
         <input type="submit" id="submit" name="speichern"><br>
+        <div><label for="image">Bild</label>
+        <input class="form-control" type="text" id="image" name="image"></div>
     </form>
 
 <?php
@@ -87,6 +91,10 @@ $stmt = $dbh->prepare('SELECT * FROM posts');
             <h4><?= htmlspecialchars($output['post_title'], ENT_QUOTES, "UTF-8"); ?></h4>
             <p><?= htmlspecialchars($output['post_text'], ENT_QUOTES, "UTF-8"); ?></p>    
             <p><?= htmlspecialchars($output['created_at'], ENT_QUOTES, "UTF-8"); ?></p>
+            <?php if( htmlspecialchars($output['post_image'], ENT_QUOTES, "UTF-8") !== ''){
+            ?><img class="post_images" src="<?= htmlspecialchars($output['post_image'], ENT_QUOTES, "UTF-8"); ?>" onError="this.src='ersatzbild.png';" alt="Bild nicht verfügbar" /><?php
+        }
+        ?>     
             <hr>
 
         </div>
@@ -95,7 +103,35 @@ $stmt = $dbh->prepare('SELECT * FROM posts');
 foreach($stmt->fetchAll() as $x) {
     var_dump($x);
 }
+
  ?>
+ <div class="wrapper">
+
+<h2 class="form-title">BLJ-Blogs</h2>
+<h4>Hier sind die anderen Blogs:</h4>
+<?php
+$user = 'guest';
+$pass = 'blj12345';
+$dbh = new PDO('mysql:host=10.20.16.101;dbname=blogdb', $user, $pass);
+
+$stmt = $dbh->prepare('SELECT * FROM andereblogs');
+$stmt->execute();
+
+foreach($stmt as $output){?>
+<div class="form-actions">
+    <a href="http://<?= htmlspecialchars($output['ip'], ENT_QUOTES, "UTF-8");
+    ?><?= htmlspecialchars($output['pfad'], ENT_QUOTES, "UTF-8");
+    ?>"><?= htmlspecialchars($output['name'], ENT_QUOTES, "UTF-8");
+    ?></a>
+    
+</div>
+<?php
+}
+?>
+</div>
+
+
+ 
  </body>
  
  </html>
