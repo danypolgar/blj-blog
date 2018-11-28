ff4222a8a45663d93280946d541b0922ab9bc12d
Daniel Polgar <danypolgar@gmail.com>
2018-11-28 16:16:31 +0100

    post read complete

--- blog/index.php
+++ blog/index.php
@@ -29,24 +29,19 @@ label, #submit {
 }
  
  </style>
-   
- <title>Blog</title>
- <meta http-equiv="content-type" content="text/html; charset=utf-8" />
- <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
- </head>
- <body>
-
-<?php
+ <?php
 $user = 'root';
 $pass = '';
 $dbh = new PDO('mysql:host=localhost;dbname=posts', $user, $pass);
 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$isNameValid = true;
+$isTitleValid = true;
+$isPostValid = true;
     
 if (isset($_POST["speichern"])) {
     $name = $_POST['name'] ??'';
     $title = $_POST['title']??'';
     $post = $_POST['post']??'';
-    ?> <div class=errordiv> <?php
     if(empty($name)){
         ?> <p class=errorbox>Bitte geben Sie Ihr Name an.</p> <?php
         $isNameValid = false;
@@ -59,26 +54,48 @@ if (isset($_POST["speichern"])) {
        ?> <p class=errorbox>Bitte schreiben Sie eine Nachticht.</p> <?php
        $isPostValid = false;
     }
-
     
 
 
-    $stmt = $dbh->prepare("INSERT INTO `posts` (created_by, post_title, post_text) VALUES(:crby, :poti, :potx)");
-    $stmt->execute([':crby' => $name, ':poti' => $title, ':potx' => $post]);
+    
+    if($isNameValid===true && $isTitleValid===true && $isPostValid===true){
+        $stmt = $dbh->prepare("INSERT INTO posts (created_by, post_title, post_text) VALUES(:name, :title, :post)");
+        $stmt->execute([':name' => $name, ':title' => $title, ':post' => $post]);
+    }
+}
+?>
 
 
 
-   } 
-   
-   ?> 
-   </div> 
+
 
      <h1>Blog</h1><br>
      <form action="index.php" method="post">
         <label for="name"> Name:</label><input id="name" type="text" name="name"><br>
         <label for="title"> Title:</label><input id="title" type="text" name="title"><br>
-        <label for="post"> Text:</label><textarea id="post" name="post" rows = "4" cols = "50"></textarea><br>
+        <label for="post"> Text:</label><textarea id="post"  name="post" rows="10"></textarea><br>
         <input type="submit" id="submit" name="speichern"><br>
     </form>
+
+<?php
+$stmt = $dbh->prepare('SELECT * FROM posts');
+    $stmt->execute();
+    
+    foreach($stmt as $output){?>
+        <div class="form-actions">
+            <h2><?= htmlspecialchars($output['created_by'], ENT_QUOTES, "UTF-8"); ?></h2>
+            <h4><?= htmlspecialchars($output['post_title'], ENT_QUOTES, "UTF-8"); ?></h4>
+            <p><?= htmlspecialchars($output['post_text'], ENT_QUOTES, "UTF-8"); ?></p>    
+            <p><?= htmlspecialchars($output['created_at'], ENT_QUOTES, "UTF-8"); ?></p>
+            <hr>
+
+        </div>
+        <?php
+    }
+foreach($stmt->fetchAll() as $x) {
+    var_dump($x);
+}
+ ?>
  </body>
+ 
  </html>
